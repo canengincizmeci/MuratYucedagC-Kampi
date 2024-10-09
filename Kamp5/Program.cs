bf7ab0ecd31c010558cda547bacb60332f4fdacb
Can Engin Çizmeci <canncizmeci@gmail.com>
2024-10-09 18:40:24 +0300

    5. Ders tamamlandı

---
+++ Kamp5/Program.cs
@@ -0,0 +1,169 @@
+﻿
+
+
+#region Alt alta 10 tane yildiz olusturma
+
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.WriteLine("*");
+//}
+
+#endregion
+
+
+#region Yan yana 10 tane yildiz olusturma
+
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.Write("*");
+//}
+
+#endregion
+
+
+#region Alt alta 10 tane yildiz olusturma her satirda 10 tane yildiz olsun
+
+
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.WriteLine("**********");
+//}
+
+
+
+#endregion
+
+#region Dik ucgen
+
+
+//for (int i = 1; i <= 5; i++)
+//{
+//    for (int j = 0; j < i; j++)
+//    {
+//        Console.Write("*");
+
+//    }
+//    Console.WriteLine();
+//}
+
+
+#endregion
+
+
+#region Ters Dik Ucgen
+
+
+//for (int i = 0; i < 10; i++)
+//{
+//	for (int j = 10; j > i; j--)
+//	{
+//        Console.Write("*");
+//	}
+//    Console.WriteLine();
+//}
+
+
+#endregion
+
+#region Dik ve Ters Dik Ucgen beraber
+
+//for (int i = 0; i <= 5; i++)
+//{
+//    for (int j = 0; j <= i; j++)
+//    {
+//        Console.Write("*");
+//    }
+//    Console.WriteLine();
+//}
+//for (int k = 4; k >= 1; k--)
+//{
+//    for (int m = 1; m <= k; m++)
+//    {
+
+//        Console.Write("*");
+
+//    }
+//    Console.WriteLine();
+
+//}
+
+#endregion
+
+
+#region Baklava Dilimi
+
+//int n = 5;
+//for (int i = 1; i <= n; i++)
+//{
+
+//    for (int j = n - 1; j > 0; j--)
+//    {
+//        Console.Write(" ");
+//    }
+//    for (int k = 1; k <= 2 * i - 1; k++)
+//    {
+//        Console.Write("*");
+
+//    }
+//    Console.WriteLine();
+//}
+//for (int i = n - 1; i >= 1; i--)
+//{
+//    for (int j = n - 1; j > 0; j--)
+//    {
+//        Console.Write(" ");
+
+//    }
+//    for (int k = 1; k <= 2 * i - 1; k++)
+//    {
+//        Console.Write("*");
+//    }
+//    Console.WriteLine();
+//}
+#endregion
+
+
+#region Piramit
+
+//int n = 5;
+//for (int i = 1; i <= n; i++)
+//{
+//    for (int j = n - i; j > 0; j--)
+//    {
+//        Console.Write(" ");
+//    }
+//    for (int k = 1; k <= 2 * i - 1; k++)
+//    {
+//        Console.Write("*");
+
+
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+#endregion
+
+#region Ters Piramit
+
+//int n = 5;
+//for (int i = n; i >= 1; i--)
+//{
+//    for (int j = n - i; j > 0; j--)
+//    {
+//        Console.Write(" ");
+//    }
+//    for (int k = 1; k <= 2 * i - 1; k++)
+//    {
+//        Console.Write("*");
+
+//    }
+//    Console.WriteLine();
+//}
+
+
+#endregion
+
+
+ 
